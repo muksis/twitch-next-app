3e186b329e3c897a182727621b7ff174be57f3a6
muksis <60219890+muksis@users.noreply.github.com>
2022-06-27 16:43:41 -0400

    Initial commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <div className="fixed h-14 w-full flex flex-nowrap items-center p-4 bg-[#0e0e10] mb-[2px] z-10">
+      
+    </div>
+  );
+}
+
+export default Navbar;
