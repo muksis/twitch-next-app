278c9c67c922f08e8ddecafdbd93ab87eb48c35d
muksis <60219890+muksis@users.noreply.github.com>
2022-07-01 13:50:11 -0400

    Add mock data from mockaroo

---
+++ data/mock-data.js
@@ -0,0 +1,103 @@
+export const rec_channels = [
+  {
+    id: 1, 
+    avatar: "https://robohash.org/temporibusuteius.png?size=50x50&set=set1",
+    username: "ccolclough0",
+    game_name: "Stringtough", 
+    rank: 57
+  },
+  {
+    id: 2,
+    avatar: "https://robohash.org/sedautvoluptatem.png?size=50x50&set=set1",
+    username: "aspall1",
+    game_name: "Subin",
+    rank: 25
+  },
+  {
+    id: 3,
+    avatar: "https://robohash.org/nemolaboriosamveniam.png?size=50x50&set=set1",
+    username: "jbuzza2",
+    game_name: "Redhold",
+    rank: 19
+  },
+  {
+    id:4,
+    avatar: "https://robohash.org/voluptasliberoet.png?size=50x50&set=set1",
+    username: "mbraybrook3",
+    game_name: "Holdlamis",
+    rank: 84
+  },
+  {
+    id: 5,
+    avatar: "https://robohash.org/itaquesedducimus.png?size=50x50&set=set1",
+    username: "cmooney4",
+    game_name: "Ventosanzap",
+    rank: 59
+  },
+  {
+    id: 6,
+    avatar: "https://robohash.org/ipsaenimsit.png?size=50x50&set=set1",
+    username: "kkalkofer5",
+    game_name: "Temp",
+    rank: 77
+  },
+  {
+    id: 7,
+    avatar: "https://robohash.org/dolorquistempora.png?size=50x50&set=set1",
+    username: "qrevely6",
+    game_name: "Bitwolf",
+    rank: 48
+  },
+  {
+    id: 8,
+    avatar: "https://robohash.org/quiaestanimi.png?size=50x50&set=set1",
+    username: "ejohannes7",
+    game_name: "Konklux",
+    rank: 25
+  },
+  {
+    id: 9,
+    avatar: "https://robohash.org/etconsequaturiusto.png?size=50x50&set=set1",
+    username: "rbickerdicke8",
+    game_name: "Ventosanzap",
+    rank: 62
+  },
+  {
+    id: 10,
+    avatar: "https://robohash.org/estadquaerat.png?size=50x50&set=set1",
+    username: "fleftwich9",
+    game_name: "Aerified",
+    rank: 40
+  }
+];
+
+export const top_channels = [
+  {
+    id: 1, 
+    avatar: "https://robohash.org/temporibusuteius.png?size=50x50&set=set1",
+    username: "ccolclough0",
+    game_name: "Stringtough", 
+    rank: 57
+  },
+  {
+    id: 2,
+    avatar: "https://robohash.org/sedautvoluptatem.png?size=50x50&set=set1",
+    username: "aspall1",
+    game_name: "Subin",
+    rank: 25
+  },
+  {
+    id: 3,
+    avatar: "https://robohash.org/nemolaboriosamveniam.png?size=50x50&set=set1",
+    username: "jbuzza2",
+    game_name: "Redhold",
+    rank: 19
+  },
+  {
+    id:4,
+    avatar: "https://robohash.org/voluptasliberoet.png?size=50x50&set=set1",
+    username: "mbraybrook3",
+    game_name: "Holdlamis",
+    rank: 84
+  }
+];
